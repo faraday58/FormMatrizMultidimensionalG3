78cee4fc13a80c3742dc7dc44d2cafca04474767
Armando Rodríguez <jarodriguezvera@gmail.com>
2020-10-26 12:05:00 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace FormMatrizMultidimensionalG3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbMatriz1 = new System.Windows.Forms.Label();
+            this.lbMatriz2 = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSeno = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rdbGrados = new System.Windows.Forms.RadioButton();
+            this.rdbRadianes = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lbMatriz1
+            // 
+            this.lbMatriz1.AutoSize = true;
+            this.lbMatriz1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatriz1.Location = new System.Drawing.Point(75, 166);
+            this.lbMatriz1.Name = "lbMatriz1";
+            this.lbMatriz1.Size = new System.Drawing.Size(64, 25);
+            this.lbMatriz1.TabIndex = 1;
+            this.lbMatriz1.Text = "label1";
+            // 
+            // lbMatriz2
+            // 
+            this.lbMatriz2.AutoSize = true;
+            this.lbMatriz2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatriz2.Location = new System.Drawing.Point(243, 166);
+            this.lbMatriz2.Name = "lbMatriz2";
+            this.lbMatriz2.Size = new System.Drawing.Size(64, 25);
+            this.lbMatriz2.TabIndex = 1;
+            this.lbMatriz2.Text = "label1";
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbResultado.Location = new System.Drawing.Point(379, 166);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(64, 25);
+            this.lbResultado.TabIndex = 1;
+            this.lbResultado.Text = "label1";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(63, 243);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(86, 57);
+            this.btnSuma.TabIndex = 3;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(219, 243);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(86, 57);
+            this.btnResta.TabIndex = 3;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(343, 243);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(86, 57);
+            this.btnMultiplicar.TabIndex = 3;
+            this.btnMultiplicar.Text = "x";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // btnSeno
+            // 
+            this.btnSeno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeno.Location = new System.Drawing.Point(63, 329);
+            this.btnSeno.Name = "btnSeno";
+            this.btnSeno.Size = new System.Drawing.Size(86, 57);
+            this.btnSeno.TabIndex = 3;
+            this.btnSeno.Text = "Sen";
+            this.btnSeno.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(475, 243);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(86, 143);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Intro";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Matriz 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(243, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Matriz 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(378, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 29);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Resultado";
+            // 
+            // rdbGrados
+            // 
+            this.rdbGrados.AutoSize = true;
+            this.rdbGrados.Location = new System.Drawing.Point(65, 26);
+            this.rdbGrados.Name = "rdbGrados";
+            this.rdbGrados.Size = new System.Drawing.Size(87, 24);
+            this.rdbGrados.TabIndex = 4;
+            this.rdbGrados.TabStop = true;
+            this.rdbGrados.Text = "Grados";
+            this.rdbGrados.UseVisualStyleBackColor = true;
+            // 
+            // rdbRadianes
+            // 
+            this.rdbRadianes.AutoSize = true;
+            this.rdbRadianes.Location = new System.Drawing.Point(221, 26);
+            this.rdbRadianes.Name = "rdbRadianes";
+            this.rdbRadianes.Size = new System.Drawing.Size(102, 24);
+            this.rdbRadianes.TabIndex = 4;
+            this.rdbRadianes.TabStop = true;
+            this.rdbRadianes.Text = "Radianes";
+            this.rdbRadianes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 434);
+            this.Controls.Add(this.rdbRadianes);
+            this.Controls.Add(this.rdbGrados);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSeno);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.lbMatriz2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbMatriz1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbMatriz1;
+        private System.Windows.Forms.Label lbMatriz2;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSeno;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rdbGrados;
+        private System.Windows.Forms.RadioButton rdbRadianes;
+    }
+}
+
---
+++ Matriz.cs
@@ -0,0 +1,38 @@
+﻿
+namespace FormMatrizMultidimensionalG3
+{
+    class Matriz
+    {
+        private int n;
+        private int m;
+
+        public int N
+        {
+            get => n;
+            set
+            {
+                if(  value < 2  )
+                {
+                    n = 2;
+                }
+                else
+                {
+                    n = value;
+                }
+                
+            }
+        }
+        public int M {
+            get => m; 
+            set {
+                if( value < 2)
+                {
+                    m = 2;
+                }
+                else
+                {
+                    m = value;
+                }                
+            }
+    }
+}
